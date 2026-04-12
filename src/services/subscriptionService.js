a25da7a83bd8af159011353bdd7032963e42342c
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-12 23:50:53 +0300

    feat(API): implemented servuce logic  for every endpoint

---
+++ src/services/subscriptionService.js
@@ -0,0 +1,69 @@
+import crypto from 'node:crypto';
+
+import {
+    isValidRepoFormat,
+    verifyRepository,
+    getLatestRelease,
+} from "./githubService.js";
+
+export async function subscribeService(reqBody) {
+    try {
+        const { email, repo } = reqBody;
+        const confirmToken = crypto.randomUUID()
+        const unsubscribeToken = crypto.randomUUID()
+
+        const data = await verifyRepository(repo);
+
+        //await surbscribeDB(data, confirmToken, unsubscribeToken);//will be for storing values into server
+
+        const returnVal = {
+            status: 201,
+            message: "Confirmation letter was sent, check your email",
+        };
+        return returnVal;
+    } catch (err) {
+        const status = err.status || 500;
+        const message = err.message || "Internal server error";
+
+        return { status, message };
+    }
+}
+
+export async function confirmSubscriptionService(token) {
+    try {
+        //await confirmSubscriptionDB(token);
+
+        return { status: 200, message: "Confirmed!" };
+    } catch (err) {
+        return { 
+            status: 500, 
+            message: err.message || "token is incorrect" 
+        };
+    }
+}
+
+export async function unsubscribeService(token) {
+    try {
+        //await unsubscribeDB(token);
+
+        return { status: 200, message: "Confirmed!" };
+    } catch (err) {
+        return { 
+            status: 500, 
+            message: err.message || "token is incorrect" 
+        };
+    }
+}
+
+export async function subscriptionsService(email) {
+    try {
+        //await selectSubscriptionsDB(email);
+
+        return { status: 200, message: "Confirmed!" };
+    } catch (err) {
+        return { 
+            status: 500, 
+            message: err.message || "token is incorrect" 
+        };
+    }
+}
