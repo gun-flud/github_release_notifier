f61ed50df6a0ce91a61eff47f8bd8a4e0b84f9bd
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-12 23:55:43 +0300

    feat(API): implemented api/unsubscribe/:token logic

---
+++ src/db/unsubscribeDB.js
@@ -0,0 +1,17 @@
+import { pool } from "./pool.js";
+
+export default async function unsubscribeDB(token) {
+    const { rows } = pool.query(
+        `
+        DELETE FROM subscriptions
+        WHERE unsubscribe_token = $1
+        RETURNING id`,
+        [token],
+    );
+
+    if (rows.length === 0) {
+        const err = new Error('Token is invalid');
+        err.status = 404;
+        throw err;
+    }
+}
