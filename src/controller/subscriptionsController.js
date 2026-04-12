28b929750400a6dd4dbcd115a94e5e9f2ef18e74
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-12 23:49:38 +0300

    feat(API): aded controller  for every endpoint

---
+++ src/controller/subscriptionsController.js
@@ -0,0 +1,30 @@
+import { 
+    subscribeService,
+    confirmSubscriptionService,
+    unsubscribeService,
+    subscriptionsService } from '../services/subscriptionService.js'
+export async function subscribe (req, res) {
+//     const response = subscribeService(req.body);
+//     res.code((await response).status).send((await response).message);
+
+    await dataHandler(subscribeService(req.body), res);
+}
+
+export async function confirmSubscription (req, res) {
+    await dataHandler(confirmSubscriptionService(req.params.token), res);
+}
+
+export async function unsubscribe (req, res) {
+    await dataHandler(unsubscribeService(req.params.token), res);
+}
+
+export async function subscriptions (req, res) {
+    await dataHandler(subscriptionsService(req.query.email), res);
+}
+
+async function dataHandler(fn, res) {
+    const response = await fn;
+
+    res.code((await response).status).send((await response).message)
+}
+
