599f8e5c0f7e49d0cfa4aff5d496321755b3b297
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-12 23:46:13 +0300

    feat(API): started fastify server

--- src/app.js
+++ src/app.js
@@ -1,16 +1,46 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import runMigrations from './db/migration.manager.js';
+
+// async function server () {
+//     try {
+//         console.log('server is working');
+
+//         await runMigrations();
+//     } catch (err) {
+//         console.error('[Error]', err.message);
+//         process.exit(1);
+//     }
+// }
+
+// server();
+
+import Fastify from 'fastify';
+// import cors from 'fastify/cors'
 import dotenv from 'dotenv';
+
+import routes from './routes/router.js';
 dotenv.config();
-import runMigrations from './db/migration.manager.js';
 
-async function server () {
-    try {
-        console.log('server is working');
+const PORT = process.env.PORT;
 
-        await runMigrations();
-    } catch (err) {
-        console.error('[Error]', err.message);
-        process.exit(1);
-    }
+const fastify = Fastify({
+    logger: true,
+});
+
+
+const port = {
+    port: PORT,
+    host: '0.0.0.0',
 }
 
-server();
+fastify.register( routes, {
+    prefix: '/api'
+});
+
+fastify.listen(port, (err, adress) => {
+    if (err) {
+        fastify.log.fatal(err);
+        process.exit(1);
+    }
+})
