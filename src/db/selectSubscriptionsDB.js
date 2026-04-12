a78b28ee62ecaa6f47244dd25177857fa3d2a273
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-12 23:57:43 +0300

    feat(API): implemented api/subscriptions logic

---
+++ src/db/selectSubscriptionsDB.js
@@ -0,0 +1,14 @@
+import { pool } from "./pool.js";
+
+export default async function selectSubscriptionsDB(email) {
+    const { rows } = await pool.query(
+        `
+        SELECT s.id, s.created_at, s.email, r.repository_name 
+        FROM subscriptions s
+        JOIN repositories r ON s.repository_id = r.id
+        WHERE s.email = $1 AND s.confirmed = TRUE`,
+        [email],
+    );
+
+    return rows;
+}
